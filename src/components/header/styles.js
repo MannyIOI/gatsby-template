2e72f93375403a921c52e2c5e01832e52e25ba12
Amanuel Teferi <aman.teferi.80@gmail.com>
2020-09-19 11:52:27 +0300

    (fix) setup font

--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -1,5 +1,15 @@
 import styled from 'styled-components'
+import headerImage from '../../images/header/Header.png'
 
 export const HeaderContainer = styled.div`
-    background: 
+    height: 100vh;
+    background: url(${headerImage});
+    background-repeat: no-repeat;
+    background-size: 100%;
+
+    div p, div h1 {
+        color: white;
+        font-family: Poppins;
+    }
+
 `
